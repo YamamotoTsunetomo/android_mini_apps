15c2f6e61cedb6985f24c4a1134ea02e34539950
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-02-17 17:37:47 +0400

    added lifecycleobserver

--- intent_stuff/app/src/main/java/com/example/intent_stuff/CameraFragment.kt
+++ intent_stuff/app/src/main/java/com/example/intent_stuff/CameraFragment.kt
@@ -2,42 +2,59 @@ package com.example.intent_stuff
 
 import android.net.Uri
 import android.os.Bundle
-import android.os.Environment
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.VideoView
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.ActivityResultRegistry
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import com.example.intent_stuff.databinding.FragmentCameraBinding
 
-class CameraFragment : Fragment() {
+class CameraFragment() : Fragment() {
     private var _binding: FragmentCameraBinding? = null
+    private lateinit var observer: LifeCycleObserver
+
 
     private val binding get() = _binding!!
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        observer = LifeCycleObserver(requireActivity().activityResultRegistry)
+        lifecycle.addObserver(observer)
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
-//
-//            binding.btnCaptureImage.setOnClickListener {
-//                btnCaptureClickListener(binding.videoView).launch("video/*")
-//            }
-//            binding.btnCaptureImage.setOnClickListener {
-//                btnCaptureClickListener(binding.imageView).launch("image/*")
-//        }
+        if (isAdded) {
+            binding.btnCaptureImage.setOnClickListener {
+                observer.setImage(binding.imageView)
+            }
+        }
         return binding.root
     }
-//    val btnCaptureClickListener = fun(v: View) =
-//        registerForActivityResult(ActivityResultContracts.GetContent()) {
-//            when (v) {
-//                is ImageView -> v.setImageURI(it)
-//                is VideoView -> v.setVideoURI(it)
-//            }
-//        }
 }
+
+class LifeCycleObserver(private val registry: ActivityResultRegistry) : DefaultLifecycleObserver {
+    private lateinit var getContent: ActivityResultLauncher<String>
+
+    private var uri: Uri? = null
+    override fun onCreate(owner: LifecycleOwner) {
+        getContent = registry.register("key", owner, ActivityResultContracts.GetContent()) {
+            uri = it
+        }
+    }
+
+    fun setImage(v: ImageView) {
+        getContent.launch("image/*")
+        v.setImageURI(uri)
+    }
+}
+
