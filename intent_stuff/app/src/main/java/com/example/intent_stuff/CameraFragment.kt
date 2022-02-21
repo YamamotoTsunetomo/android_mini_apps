fa9f9bed8cd162d6119de22b4747d5395ccdbbbf
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-02-21 15:45:24 +0400

    registerForActivityResult

--- intent_stuff/app/src/main/java/com/example/intent_stuff/CameraFragment.kt
+++ intent_stuff/app/src/main/java/com/example/intent_stuff/CameraFragment.kt
@@ -1,24 +1,12 @@
 package com.example.intent_stuff
 
-import android.app.Activity
-import android.content.Context
-import android.content.Intent
-import android.graphics.Bitmap
 import android.net.Uri
 import android.os.Bundle
-import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ImageView
-import androidx.activity.result.ActivityResultLauncher
-import androidx.activity.result.ActivityResultRegistry
-import androidx.activity.result.contract.ActivityResultContract
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityOptionsCompat
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.DefaultLifecycleObserver
-import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.MutableLiveData
 import com.example.intent_stuff.databinding.FragmentCameraBinding
 
@@ -27,29 +15,21 @@ class CameraFragment : Fragment() {
     private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        if (resultCode == Activity.RESULT_OK && requestCode == 123) {
-            val b = data?.extras?.get("data") as Bitmap
-            binding.imageView.setImageBitmap(b)
-        }
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        binding.btnCaptureImage.setOnClickListener {
-            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-            startActivityForResult(intent, 123)
-        }
-    }
-
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
+        val startForResult = registerForActivityResult(ActivityResultContracts.GetContent()) {
+            binding.imageView.setImageURI(it)
+        }
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
+        binding.btnCaptureImage.setOnClickListener {
+            startForResult.launch("image/*")
+        }
         return binding.root
+
     }
 }
 
