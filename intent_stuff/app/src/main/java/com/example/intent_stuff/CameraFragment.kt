7adb5512325742ee068d312407e1d217a11c331f
YamamotoTsunetomo <arlongtwentylemons@gmail.com>
2022-02-21 16:29:02 +0400

    registerForActivityResult

--- intent_stuff/app/src/main/java/com/example/intent_stuff/CameraFragment.kt
+++ intent_stuff/app/src/main/java/com/example/intent_stuff/CameraFragment.kt
@@ -2,13 +2,15 @@ package com.example.intent_stuff
 
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.FileProvider
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.MutableLiveData
 import com.example.intent_stuff.databinding.FragmentCameraBinding
+import java.io.File
 
 class CameraFragment : Fragment() {
 
@@ -16,17 +18,26 @@ class CameraFragment : Fragment() {
     private val binding get() = _binding!!
 
 
+    private var uri: Uri? = null
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-
-        val startForResult = registerForActivityResult(ActivityResultContracts.GetContent()) {
-            binding.imageView.setImageURI(it)
+        val camera = registerForActivityResult(ActivityResultContracts.TakePicture()) {
+            Log.d("CAMERA", it.toString())
+            Log.d("CAMERA", uri.toString())
+            if (it)
+                binding.imageView.setImageURI(uri)
         }
         _binding = FragmentCameraBinding.inflate(inflater, container, false)
+
         binding.btnCaptureImage.setOnClickListener {
-            startForResult.launch("image/*")
+//            val path = File("my_images")
+//            val file = File(path, "default_image.jpg")
+//            val uri =
+//                context?.let { it1 -> FileProvider.getUriForFile(it1, "com.example.intent_stuff.fileprovider", file) }
+//            camera.launch(uri)
         }
         return binding.root
 
